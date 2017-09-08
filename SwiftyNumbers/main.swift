9ee24bca16fab929682ec9f760903282d89027d5
NoahPeeters <noah.peeters@icloud.com>
2017-09-08 11:07:48 +0200

    Initial Commit

---
+++ SwiftyNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftyNumbers
+//
+//  Created by Noah Peeters on 08.09.17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
